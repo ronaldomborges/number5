e33a2f9b75c99f42a3575fdd3e2c5beac5aa3676
ronaldomborges <65875791+ronaldomborges@users.noreply.github.com>
2021-04-08 21:53:35 -0300

    cont1

---
+++ sript.js
@@ -0,0 +1,33 @@
+function contar() {
+  let ini = document.querySelector('#txti')
+  let fim = document.querySelector('#txtf')
+  let passo = document.querySelector('#txtp')
+  let res = document.querySelector('#res')
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.style.color = "white"
+        res.innerHTML = `<p>Imposível contar!</p>`
+    }  else {
+         res.innerHTML = `<p>Contando:</p> <br>`
+         let i = Number(ini.value)
+         let f = Number(fim.value)
+         let p = Number(passo.value)
+           if (p <= 0) {
+               window.alert(`Passo inválido! Considerando PASSO 1`)
+               p = 1
+           }  if (i <= f) {
+                 let c = 1
+                 do {
+                     res.style.color = "white"
+                     res.innerHTML += ` ${c} \u{1f449}`
+                     c += p
+                 } while (c <= f)
+              } else {
+                let c = i
+               do {
+                    res.style.color = "white"
+                    res.innerHTML += ` ${c} \u{1F449}`
+                    c -= p
+                }  while (c >= f)
+              }
+       }
+}
